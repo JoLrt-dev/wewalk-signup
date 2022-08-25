7028cc8731e09358c7d83d5aa9555e6e524faf44
Jordane Laurent <j.laurent1993@gmail.com>
2022-08-25 15:43:33 +0200

    signIn component ok

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Typography } from "@mui/material";
 import Link from "@mui/material/Link";
 
-export default function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            <Link color="inherit" href="https://maps.google.fr">
-                WeWalk</Link>
-            {' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>)
-};
+export default function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      <Link color="inherit" href="https://maps.google.fr">
+        WeWalk
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,19 +1,108 @@
-import {createTheme, ThemeProvider} from '@mui/material/styles';
-import { Container } from '@mui/system';
 import React from "react";
-import Copyright from './Copyright';
+import Copyright from "./Copyright";
 
+import {
+  Avatar,
+  Box,
+  CssBaseline,
+  TextField,
+  Button,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Link,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Container } from "@mui/system";
+import { Typography } from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const theme = createTheme();
 
 export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Container>
-                <Copyright sx={{ mt: 5 }} />
-            </Container>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />{" "}
+        <Box
+          sx={{
+            marginTop: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign In
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              label="Password"
+              name="password"
+              autoComplete="current-password"
+              autoFocus
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password ?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Don't have an account ? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
+
+// See to add the sx props and configurate the createTheme
